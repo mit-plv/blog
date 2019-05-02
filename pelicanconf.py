131403b7c0e71d7d8ef3f744a917450a67b20537
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-05-02 16:54:57 -0400

    Initial Pelican setup

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'PLV'
+SITENAME = 'PLV@MIT'
+SITEURL = ''
+SITESUBTITLE = "Updates from Adam Chlipala's PLV group at MIT"
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('PLV', 'http://plv.csail.mit.edu/'),
+         ('Bedrock', 'http://plv.csail.mit.edu/bedrock/'),
+         ('Fiat', 'http://plv.csail.mit.edu/fiat/'),
+         ('Ur/Web', 'http://plv.csail.mit.edu/bedrock/ur/'),
+         ('Kami', 'http://plv.csail.mit.edu/kami/'),)
+
+# Social widget
+SOCIAL = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
