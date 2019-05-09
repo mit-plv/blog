d449705e97c6d0d91feeac1d82d1ea20b7455378
Clément Pit-Claudel <clement.pitclaudel@live.com>
2019-05-09 11:55:04 -0400

    theme: Switch to Iosevka and vendor fonts

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,40 +7,17 @@ SITENAME = 'PLV@MIT'
 SITEURL = ''
 FEED_DOMAIN = SITEURL
 
-THEME = 'flex-theme'
-
-## Flex-theme specific ##
-SITETITLE = SITENAME
-SITESUBTITLE = SITEDESCRIPTION = "Updates from Adam Chlipala's PLV group at MIT"
-
-BROWSER_COLOR = '#333'
-SITELOGO = SITEURL + 'static/plv.png' # FIXME
-
-MAIN_MENU = True
-ROBOTS = 'index, follow'
-
-COPYRIGHT_YEAR = 2019
-CC_LICENSE = {
-    'name': 'Creative Commons Attribution',
-    'version': '4.0',
-    'slug': 'by'
-}
-
-PYGMENTS_STYLE = 'emacs'
-
-MENUITEMS = (('PLV (main site)', 'http://plv.csail.mit.edu/'),)
-#########################
-
-PATH = 'content'
-THEME_TEMPLATES_OVERRIDES = []
-STATIC_PATHS = ['images', 'static']
-CUSTOM_CSS = 'static/custom.css'
-EXTRA_PATH_METADATA = {}
-
-FILENAME_METADATA = r'(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
-
+LOCALE = ('usa', 'en_US', 'en_US.utf8')
 TIMEZONE = 'America/New_York'
 DEFAULT_LANG = 'en'
+
+PATH = 'content'
+THEME_TEMPLATES_OVERRIDES = ['templates']
+STATIC_PATHS = ['images', 'static']
+CUSTOM_CSS = None # Set using a template override (custom_head.html)
+FILENAME_METADATA = r'(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
+
+THEME = 'flex-theme'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -58,8 +35,38 @@ SOCIAL = ()
 
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
+# Document-relative URLs when developing
 RELATIVE_URLS = True
+
+## Flex-theme specific ##
+SITETITLE = SITENAME
+SITESUBTITLE = SITEDESCRIPTION = "Updates from Adam Chlipala's PLV group at MIT"
+
+BROWSER_COLOR = '#333'
+USE_GOOGLE_FONTS = False
+
+MAIN_MENU = True
+ROBOTS = 'index, follow'
+
+COPYRIGHT_YEAR = 2019
+COPYRIGHT_NAME = 'PLV @ MIT'
+CC_LICENSE = None
+# {
+#     'name': 'Creative Commons Attribution',
+#     'version': '4.0',
+#     'slug': 'by',
+#     'local_icons': True
+# }
+
+PYGMENTS_STYLE = 'emacs'
+
+MENUITEMS = (('PLV (main site)', 'http://plv.csail.mit.edu/'),)
+
+
+EXTRA_PATH_METADATA = {
+    'static/plv.png': {'path': 'theme/img/profile.png'}
+}
+#########################
 
 ## Alectryon support ##
 import sys
@@ -68,7 +75,6 @@ sys.path.insert(0, os.path.dirname(__file__))
 from alectryon.externals import docutils_support
 
 docutils_support.register()
-THEME_TEMPLATES_OVERRIDES.append('templates/')
 STATIC_PATHS.append('../alectryon/alectryon.css')
 EXTRA_PATH_METADATA['../alectryon/alectryon.css'] = {'path': 'static/alectryon.css'}
 #######################
